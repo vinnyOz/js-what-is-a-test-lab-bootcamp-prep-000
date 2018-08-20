849c0f5221cceba91436d6786e0b57b021d19893
vincent ndisi <vndis1298@yahoo.com>
2018-08-20 23:58:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ var name = "Susan";
 var height = 20;
 var message ='is ${height} inches tall';
 var message =' ${name} is ${height} inches tall';
-if ()
+if (name<height) {
+   alert("height is correct")
+}
 module.exports = {name, height, message};
 
